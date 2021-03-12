79750d356b91071dffff21fcb7f6d3008aeb9804
TheCaptain989 <thecaptain989@protonmail.com>
2021-03-11 19:36:08 -0600

    Update
    - Renamed variables so reduce change of environment collision
    - Added additional error handling
    - Added comments to code
    - Updated documentation

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-# Bazarr cleansubs
+# About
 A shell script to automatically remove common annoying scene branding and attribution entries from subtitle files.
 Only .SRT format subtitles are supported.
 
-## Usage
-
-1. Place the `cleansubs.sh` shell script file in the root directory of Bazarr
-2. Configure a custom script from the Bazarr Settings->Subtitles screen to call:
+# Installation
+1. Place the `cleansubs.sh` shell script file in the root directory of Bazarr, usually `/config`
+2. Configure a custom script from the Bazarr *Settings* > *Subtitles* screen to call:
 
    **`/config/cleansubs.sh "{{subtitles}}" ;`**
 
    from the **Post-processing command** field.  **Note the double quotes!**
 
-**NOTE:** The original subtitle file will be deleted/overwritten and permanently lost.
+   *Example*  
+   ![cleansubs](.assets/bazarr-settings-subtitles.png)
+
+
+   >**NOTE:** The original subtitle file will be deleted/overwritten and permanently lost.
+
+## Usage
+The script is not configurable.  
+The string matching only supports English at this time.
 
 ### Syntax
-
 The script accepts one argument:
 
 `<subtitle_file>`
 
-The argument is the full path and file name to a subtitle file.
-
-![cleansubs](.assets/bazarr-settings-subtitles.png)
+The argument is the full path and file name to a subtitle file. In Bazarr, you should use the `{{subtitles}}` variable.
 
 ### Logs
 Logs are integrated into the Bazarr log file. The text output of the script is designed to be compatible with the Bazarr log file format. This has been tested with Bazarr version(s):
@@ -38,6 +42,8 @@ Removed subtitle entries are included in the Bazarr "Exception" details of each 
 
 ___
 # Credits
+
+This would not be possible without the following:
 
 [Bazarr](https://www.bazarr.media/)  
 [LinuxServer.io Bazarr](https://hub.docker.com/r/linuxserver/bazarr) container
--- cleansubs.sh
+++ cleansubs.sh
@@ -2,7 +2,8 @@
 
 # Script to remove common annoying scene branding and attribution from subtitle files
 #  https://github.com/TheCaptain989/bazarr-cleansubs
-# Works on properly formatted SRT files
+# Works on properly formatted SRT files.
+# RegEx is English only.
 # 
 # Output is designed to be compatible with the Bazarr log file format
 # Removed subtitle entries are included after the pipe symbol (|) and included in the Bazarr "Exception" details of each log entry
@@ -17,9 +18,12 @@
 #  5 - subtitle file not found
 #  6 - unknown subtitle file type
 # 10 - awk script failed to write temporary subtitle file
+# 11 - awk script failed in an unknown way
 
-TEMPSUB="$1.tmp"
-SUB="$1"
+### Variables
+export cleansubs_tempsub="$1.tmp"
+export cleansubs_sub="$1"
+
 # Parking here for future use.
 #RECYCLEBIN=$(python3 -c "import sqlite3
 #conSql = sqlite3.connect('/config/db/bazarr.db')
@@ -28,32 +32,38 @@ SUB="$1"
 #print(cursorObj.fetchone()[0])
 #conSql.close()")
 
-if [ -z "$SUB" ]; then
+# Check for required command line argument
+if [ -z "$cleansubs_sub" ]; then
   MSG="\nERROR: No subtitle file specified! Not called from Bazarr?"
   echo -n "$MSG"
   exit 1
 fi
 
-if [ ! -f "$SUB" ]; then
-  MSG="\nERROR: Input file not found: \"$SUB\""
+# Check for existence of subtitle file
+if [ ! -f "$cleansubs_sub" ]; then
+  MSG="\nERROR: Input file not found: \"$cleansubs_sub\""
   echo -n "$MSG"
   exit 5
 fi
 
-if [[ "$SUB" != *.srt ]]; then
+# Check if subtitle is in the expected format
+if [[ "$cleansubs_sub" != *.srt ]]; then
   # This script only works on SRT subtitles
-  MSG="\nERROR: Expected SRT file. Incorrect file suffix: \"$SUB\""
+  MSG="\nERROR: Expected SRT file. Incorrect file suffix: \"$cleansubs_sub\""
   echo -n "$MSG"
   exit 6
 fi
 
-until [ ! -s "$TEMPSUB" ]; do
+# Generate temporary file name
+until [ ! -s "$cleansubs_tempsub" ]; do
   # Temporary file already exists
   i=$((i+1))
-  TEMPSUB="${TEMPSUB%%.*}.srt.$i.tmp"
+  cleansubs_tempsub="${cleansubs_tempsub%%.*}.srt.$i.tmp"
 done
 
-cat "$SUB" | dos2unix | awk '
+#### BEGIN MAIN
+cat "$cleansubs_sub" | dos2unix | awk -v SubTitle="$cleansubs_sub" \
+-v TempSub="$cleansubs_tempsub" '
 function escape_html(str){
   # escape HTML in subs
   gsub(/&/,"\\&amp;",str);gsub(/</,"\\&lt;",str);gsub(/>/,"\\&gt;",str);gsub(/"/,"\\&quot;",str)
@@ -63,62 +73,79 @@ BEGIN {
   RS=""     # one or more blank lines
   FS="\n"
   IGNORECASE=1
-  MSGMAIN="<br/>cleansubs.sh<br/>Subtitle file: '"$SUB"'<br/>"
-  TempSub="'"$TEMPSUB"'"
+  # Start a new log entry
+  MSGMAIN="<br/>cleansubs.sh<br/>Subtitle file: " SubTitle "<br/>"
   indexdelta=0
 }
+# Check for Byte Order Mark
 $1 ~ /^\xef\xbb\xbf/ {
-  if (NR == 1) { sub(/^\xef\xbb\xbf/,"") }    # Remove BOM for UTF-8 encoding
+  # Remove BOM from UTF-8 encoded file
+  if (NR == 1) { sub(/^\xef\xbb\xbf/,"") }
 }
+# Get entry number
 $1 ~ /^[0-9]+$/ {
   Entry=$1
   sub(Entry,"")
   Entry = Entry + indexdelta
 }
+# Get timestamp
 $2 ~ /^[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2},[0-9]{1,3} --> [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2},[0-9]{1,3}$/ {
   Timestamp=$2
   sub(Timestamp,""); sub(/\n\n/,"")
 }
-/(subtitle[sd]? )?(precisely )?((re)?sync(ed|hronized)?|translation( and review)?|encoded|improved|provided|edited|production|created|extracted)( (&|and) correct(ed|ions))?( by|:) .*<\/font|opensubtitles|subscene|subtext:|purevpn|english (subtitles|- sdh)|trailers\.to/ {
+# Match on objectionable strings
+# /(subtitle[sd]? )?(precisely )?((re)?sync(ed|hronized)?|translation( and review)?|encoded|improved|provided|edited|production|created|extracted)( (&|and) correct(ed|ions))?( by|:) .*<\/font|opensubtitles|subscene|subtext:|purevpn|english (subtitles|- sdh)|trailers\.to/ {
   gsub(/\n/,"<br/>")
   MSGEXT=MSGEXT"Removing entry " (Entry - indexdelta)": " escape_html($0) "<br/>"
   indexdelta -= 1
   next
 }
+# Do this for every line
 {
   if (Entry && Timestamp) {
+    # Generate a newly renumbered entry
     Newentry[++Entries] = Entry "\n" Timestamp "\n" $0
     Entry=""
     Timestamp=""
   } else {
-    MSGEXT=MSGEXT"Skipping malformed entry " NR". Entry: " Entry,Timestamp,escape_html($0) "<br/>"
+    # Add to Bazarr Exception log
+    MSGEXT=MSGEXT "Skipping malformed entry " NR ". Entry: " Entry,Timestamp,escape_html($0) "<br/>"
   }
 }
 END {
   if (NR == Entries) {
-    MSGMAIN=MSGMAIN"No changes to subtitle file required. Total entries scanned: " NR
-    printf MSGMAIN"|"MSGEXT
+    # No changes to file
+    MSGMAIN=MSGMAIN "No changes to subtitle file required. Total entries scanned: " NR
+    printf MSGMAIN "|" MSGEXT
     exit 1
   }
-  MSGMAIN=MSGMAIN"Original entries: " NR ". Total entries kept: " Entries
-  MSGEXT=MSGEXT"Writing new temporary file: "TempSub""
-  printf MSGMAIN"|"MSGEXT
+  # Write new subtitle file
+  MSGMAIN=MSGMAIN "Original entries: " NR ". Total entries kept: " Entries
+  MSGEXT=MSGEXT "Writing new temporary file: " TempSub ""
+  printf MSGMAIN "|" MSGEXT
   for (i = 1; i <= Entries; i++)
     print Newentry[i] "\n" >> TempSub
   close(TempSub)
 }'
 
+#### END MAIN
+
 RC="${PIPESTATUS[2]}"  # captures awk exit status
 
 if [ $RC == "0" ]; then
   # Check for script completion and non-empty file
-  if [ -s "$TEMPSUB" ]; then
-    mv "$TEMPSUB" "$SUB"
-    MSG="<br/>Subtitle cleaned: $SUB"
+  if [ -s "$cleansubs_tempsub" ]; then
+    mv "$cleansubs_tempsub" "$cleansubs_sub"
+    MSG="<br/>Subtitle cleaned: $cleansubs_sub"
     echo -n "$MSG"
   else
-    MSG="<br/>ERROR: Script failed. Unable to locate or invalid file: \"$TEMPSUB\""
+    MSG="<br/>ERROR: Script failed. Unable to locate or invalid file: \"$cleansubs_tempsub\""
     echo -n "$MSG"
    exit 10
   fi
+else
+  # awk script failed in an unknown way
+  MSG="<br/>ERROR: Script encountered an unknown error processing subtitle: $cleansubs_sub"
+  echo -n "$MSG"
+  exit 11
 fi
